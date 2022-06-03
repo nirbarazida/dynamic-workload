ac9055a70e8cde90f41d73e75fafc376d19a2ef4
Nir Barazida <nir@dagshub.com>
2022-06-03 09:39:36 +0300

    implement worker

---
+++ worker/app.py
@@ -0,0 +1,35 @@
+from flask import Response, Flask, render_template, request, redirect
+import requests
+from const import LB_URL, TIME_OUT #TODO: add const file
+
+def work(buffer, iterations):
+    # do something
+    pass
+
+
+def shutdown():
+    # TODO: when launching instance add role EC2FullAccess
+    # TODO: pip install ec2-metadata
+
+    import boto3
+    from ec2_metadata import ec2_metadata
+    instance_region, instance_id = ec2_metadata.region, ec2_metadata.instance_id
+
+    ec2 = boto3.resource('ec2', region_name=instance_region)
+    instance = ec2.Instance(instance_id)
+
+    instance.terminate()
+
+
+def main():
+    while True:
+        buffer, iterations = None, None # TODO: change to func var
+        response = requests.get(LB_URL, timeout=TIME_OUT)
+        #TODO: change to if response is None
+        if response:
+            work(response, iterations)
+        else:
+            shutdown()
+
+if __name__ == '__main__':
+    main()
