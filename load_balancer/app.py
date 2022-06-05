96cd1035549b6a0874f802d7c350beaaea160f98
Nir Barazida <nir@dagshub.com>
2022-06-05 07:40:40 +0300

    add reg

--- load_balancer/app.py
+++ load_balancer/app.py
@@ -28,7 +28,7 @@ def check_time_first_in_line():
 
 
 def fire_worker(app_path, min_count=1,max_count=1):
-    client = boto3.client('ec2')
+    client = boto3.client('ec2', region_name='eu-central-1') # TODO: get user's region_name
     response = client.run_instances(ImageId=WORKER_AMI_ID,
                                     InstanceType=INSTANCE_TYPE,
                                     MaxCount=max_count,
