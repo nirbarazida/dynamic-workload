4e20d4b2dc314d3df55d1c09cf8c767dedabc18c
Nir Barazida <nir@dagshub.com>
2022-06-05 04:55:18 +0300

    Modify worker

--- worker/app.py
+++ worker/app.py
@@ -1,38 +1,22 @@
 import requests
 import boto3
 from ec2_metadata import ec2_metadata
-#TODO: add const file
-from const import LB_URL, TIME_OUT
+import os
+from const import LB_PUBLIC_IP, TIME_OUT, PORT
 
 def work(buffer, iterations):
-    # do something
+    # TODO: do something
     pass
-
-
-def shutdown():
-    # TODO: when launching instance add role EC2FullAccess
-    # TODO: pip install ec2-metadata
-
-
-    instance_region, instance_id = ec2_metadata.region, ec2_metadata.instance_id
-
-    ec2 = boto3.resource('ec2', region_name=instance_region)
-    instance = ec2.Instance(instance_id)
-
-    instance.terminate()
-
 
 def main():
     while True:
-        # TODO: Uncomment
-        # buffer, iterations = None, None # TODO: change to func var
-        # response = requests.get(LB_URL, timeout=TIME_OUT)
-        # #TODO: change to if response is None
-        # if response:
-        #     work(response, iterations)
-        # else:
-        #     shutdown()
-        pass
+        buffer, iterations = None, None # TODO: change to func var
+        response = requests.get(f'http://{LB_PUBLIC_IP}:{PORT}/get_job', timeout=TIME_OUT)
+        if response:
+            buffer = response.json()
+            work(response, iterations)
+        else:
+            os.system('sudo shutdown -h now')
 
 if __name__ == '__main__':
     main()
