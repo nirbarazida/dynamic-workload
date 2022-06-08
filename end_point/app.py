acd6c13efa4da2583a93308df6856a608cd5eaaa
Nir Barazida <nir@dagshub.com>
2022-06-08 12:57:22 -0400

    change put method

--- end_point/app.py
+++ end_point/app.py
@@ -9,9 +9,10 @@ app = Flask(__name__)
 @app.route('/enqueue', methods=['PUT'])
 def enqueue():
     if request.method == "PUT":
-        res = requests.put(url=f"http://{LB_PUBLIC_IP}/add_job_to_q",
-                           data=request.get_data(),
-                           params={"iterations": int(request.args.get("iterations"))})
+        iterations = int(request.args.get("iterations"))
+        data_file = request.get_data()
+        res = requests.put(url=f"http://{LB_PUBLIC_IP}/add_job_to_q?iterations={iterations}",
+                           data=data_file)
         # TODO: is res.status valid?
         return Response(status=res.status)
 
@@ -19,6 +20,7 @@ def enqueue():
 @app.route('/pullCompleted', methods=['POST'])
 def pullCompleted():
     if request.method == "POST":
+        # TODO: implement top
         res = requests.post(f"http://{LB_PUBLIC_IP}/pullCompleted")
         last_job = res.get_json()
         return Response(mimetype='application/json',
